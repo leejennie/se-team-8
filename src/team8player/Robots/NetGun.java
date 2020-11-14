6611a5042423f354278b5755755bb6bef7845082
18xxperson <46502267+18xxperson@users.noreply.github.com>
2020-11-14 14:06:05 -0800

    Update NetGun.java

--- src/team8player/Robots/NetGun.java
+++ src/team8player/Robots/NetGun.java
@@ -1,6 +1,7 @@
 package team8player.Robots;
 
 import battlecode.common.GameActionException;
+import battlecode.common.GameConstants;
 import battlecode.common.RobotController;
 import static team8player.Globals.*;
 
@@ -17,7 +18,7 @@ public class NetGun extends Unit {
     @Override
     public void run() throws GameActionException {
         super.run();
-        for (int i = 0; i < 15; i++) {
+        for (int i = 0; i < GameConstants.NET_GUN_SHOOT_RADIUS_SQUARED; i++) {
             if (rc.canShootUnit(i))
                 rc.shootUnit(i);
         }

707fb7555f6bd4e8b96333d7988bbed9a5687d44
Jennie <ljennie@pdx.edu>
2020-11-15 19:37:53 -0800

    updating folder with test

---
+++ test/team8playertest/robottest/DeliveryDroneTest.java
@@ -0,0 +1,27 @@
+package team8playertest.robottest;
+
+import team8player.Robots.DeliveryDrone;
+import battlecode.common.*;
+import org.junit.Test;
+import org.mockito.Mockito;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import org.mockito.Mock;
+
+public class DeliveryDroneTest {
+
+    @Test
+    public void testDeliveryDroneCreation() {
+        DeliveryDrone dd1 = Mockito.mock(DeliveryDrone.class);
+        DeliveryDrone dd2 = Mockito.mock(DeliveryDrone.class);
+        assertNotEquals(dd1, dd2);
+    }
+
+    @Test
+    public void testRun() throws GameActionException {
+        DeliveryDrone dd = Mockito.mock(DeliveryDrone.class);
+        doNothing().when(dd).run();
+        verify(dd, times(1)).run();
+        dd.run();
+    }
+}
